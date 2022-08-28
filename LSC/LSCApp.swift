d09b11b4eae521bccb7ddd81c0bef6280fa4a65b
Howon Kim <journal-02-permit@icloud.com>
2022-08-27 17:20:19 -0700

    Initialize Project

---
+++ LSC/LSCApp.swift
@@ -0,0 +1,17 @@
+//
+//  LSCApp.swift
+//  LSC
+//
+//  Created by Howon Kim on 8/27/22.
+//
+
+import SwiftUI
+
+@main
+struct LSCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
